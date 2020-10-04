558b6d4538ae6282672fb2d74bc7b17191764082
Pavithra Vijay <psv@google.com>
2020-10-04 13:19:46 -0700

    project 4

---
+++ projects/04/Fill.asm
@@ -0,0 +1,88 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Fill.asm
+
+// Runs an infinite loop that listens to the keyboard input.
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel;
+// the screen should remain fully black as long as the key is pressed. 
+// When no key is pressed, the program clears the screen, i.e. writes
+// "white" in every pixel;
+// the screen should remain fully clear as long as no key is pressed.
+
+// Put your code here.
+
+// Variables:
+// KBD
+// SCREEN
+// color = RAM[16]
+// n = RAM[17]
+// i = RAM[18]
+// temp = RAM[19]
+
+// Labels:
+// INFINITE, ZERO, ONE, SETPIXELS
+
+
+@color
+M=0              // Set word=0
+
+(INFINITE)
+
+  @KBD
+  D=M            // Get KBD value
+
+  @ZERO
+  D;JEQ          // If no key is pressed, set word=0
+
+  @ONE
+  D;JNE          // If a key is pressed, set word=-1
+
+  (ZERO)
+    @color
+    M=0          // Set word=0
+    @SETPIXELS
+    0;JMP
+
+  (ONE)
+    @color
+    M=-1         // Set word=-11
+
+  (SETPIXELS)
+    @8192        // Set n=8192
+    D=A
+    @n
+    M=D
+
+    @i           // Set i=0
+    M=0
+
+    (LOOP)       // Word loop
+      @i         // if i==n exit
+      D=M
+      @n
+      D=D-M
+      @INFINITE
+      D;JEQ
+
+      @i         // A is the word to be set
+      D=M
+      @SCREEN
+      D=D+A
+
+      @temp      // temp is the work to be set
+      M=D
+
+      @color
+      D=M        // D=color
+
+      @temp      // A is the word to be set
+      A=M
+      M=D
+
+
+      @i         // i=i+1
+      M=M+1
+      @LOOP
+      0;JMP
---
+++ projects/04/Mult.asm
@@ -0,0 +1,58 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+
+// Put your code here.
+
+  @R2      // M=R2
+  M=0      // R2=0
+
+  @R0      // M=R0
+  D=M      // D=R0
+
+  @END
+  D;JLE    // if R0 <= 0 exit
+
+  @R1      // M=R1
+  D=M      // D=R1
+
+  @END
+  D;JLE    // if R1<=0 exit
+
+  @i       // M=i (RAM[16])
+  M=0      // i=0
+
+(LOOP)
+
+  @R1      // M=R1
+  D=M      // D=R1
+
+  @i       // M=i
+  D=D-M    // D=R1-i
+
+  @END
+  D;JLE    // if i>R1 END 
+
+  @i       // M=i
+  M=M+1    // i=i+1
+
+  @R2      // M=R2
+  D=M      // D=R2
+
+  @R0      // M=R0
+  D=D+M    // D=D+R0
+
+  @R2      // M=R2
+  M=D      // R2=D
+
+  @LOOP
+  0;JMP
+
+
+(END)
+  @END
+  0;JMP
